14f34b4a3e5c12549c083c7f8b20455ceda8de56
marko-bekhta <marko.prykladna@gmail.com>
2024-10-09 11:06:29 +0200

    Update REST logging config

--- src/main/java/org/hibernate/infra/sync/jira/service/jira/client/JiraRestClientBuilder.java
+++ src/main/java/org/hibernate/infra/sync/jira/service/jira/client/JiraRestClientBuilder.java
@@ -1,10 +1,19 @@
 package org.hibernate.infra.sync.jira.service.jira.client;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.hibernate.infra.sync.jira.JiraConfig;
 
+import org.jboss.resteasy.reactive.client.api.ClientLogger;
+import org.jboss.resteasy.reactive.client.api.LoggingScope;
+
+import io.quarkus.logging.Log;
 import io.quarkus.rest.client.reactive.QuarkusRestClientBuilder;
+import io.vertx.core.MultiMap;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpClientResponse;
 
 public class JiraRestClientBuilder {
 
@@ -13,14 +22,82 @@ public class JiraRestClientBuilder {
 
 		Map<String, String> headers = jira.loginKind().headers(jiraUser.email(), jiraUser.token());
 
-		return QuarkusRestClientBuilder.newBuilder().baseUri(jira.apiUri())
-				// TODO: Add a custom client logger that cleans up auth headers so that we do
-				// not write any secrets to the logs ...
+		QuarkusRestClientBuilder builder = QuarkusRestClientBuilder.newBuilder().baseUri(jira.apiUri())
 				.clientHeadersFactory((incomingHeaders, clientOutgoingHeaders) -> {
 					for (var entry : headers.entrySet()) {
 						clientOutgoingHeaders.add(entry.getKey(), entry.getValue());
 					}
 					return clientOutgoingHeaders;
-				}).build(JiraRestClient.class);
+				});
+		if (jira.logRequests()) {
+			builder.clientLogger(CustomClientLogger.INSTANCE).loggingScope(LoggingScope.REQUEST_RESPONSE);
+		}
+		return builder.build(JiraRestClient.class);
+	}
+
+	private static class CustomClientLogger implements ClientLogger {
+		private static final CustomClientLogger INSTANCE = new CustomClientLogger();
+
+		@Override
+		public void setBodySize(int bodySize) {
+			// ignored
+		}
+
+		@Override
+		public void logResponse(HttpClientResponse response, boolean redirect) {
+			response.bodyHandler(body -> Log.infof("%s: %s %s, Status[%d %s], Headers[%s], Body:\n%s",
+					redirect ? "Redirect" : "Response", response.request().getMethod(),
+					response.request().absoluteURI(), response.statusCode(), response.statusMessage(),
+					asString(response.headers()), bodyToString(body)));
+		}
+
+		@Override
+		public void logRequest(HttpClientRequest request, Buffer body, boolean omitBody) {
+			if (omitBody) {
+				Log.infof("Request: %s %s Headers[%s], Body omitted", request.getMethod(), request.absoluteURI(),
+						asString(request.headers()));
+			} else if (body == null || body.length() == 0) {
+				Log.infof("Request: %s %s Headers[%s], Empty body", request.getMethod(), request.absoluteURI(),
+						asString(request.headers()));
+			} else {
+				Log.infof("Request: %s %s Headers[%s], Body:\n%s", request.getMethod(), request.absoluteURI(),
+						asString(request.headers()), bodyToString(body));
+			}
+		}
+
+		private String bodyToString(Buffer body) {
+			if (body == null) {
+				return "";
+			} else {
+				return body.toString();
+			}
+		}
+
+		private String asString(MultiMap headers) {
+			if (headers.isEmpty()) {
+				return "";
+			}
+			StringBuilder sb = new StringBuilder();
+			boolean isFirst = true;
+			for (Map.Entry<String, String> entry : headers) {
+				if (isFirst) {
+					isFirst = false;
+				} else {
+					sb.append(' ');
+				}
+				sb.append(entry.getKey()).append('=').append(entry.getValue());
+			}
+			return headers.entries().stream()
+					.map(entry -> "%s: %s".formatted(entry.getKey(), sanitize(entry.getKey(), entry.getValue())))
+					.collect(Collectors.joining(" | "));
+		}
+
+		private String sanitize(String header, String value) {
+			if ("Authorization".equalsIgnoreCase(header)) {
+				return "***";
+			} else {
+				return value;
+			}
+		}
 	}
 }
