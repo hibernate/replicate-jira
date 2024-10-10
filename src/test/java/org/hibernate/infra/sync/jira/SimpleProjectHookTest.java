21187c5a3e0ab1cd6f0fd736f5ffcba843a88d31
marko-bekhta <marko.prykladna@gmail.com>
2024-10-10 16:32:58 +0200

    Add webhook body signature check

--- src/test/java/org/hibernate/infra/sync/jira/SimpleProjectHookTest.java
+++ src/test/java/org/hibernate/infra/sync/jira/SimpleProjectHookTest.java
@@ -4,6 +4,8 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.hibernate.infra.sync.jira.service.validation.RequestSignatureFilter;
+
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -11,51 +13,43 @@ import io.restassured.http.ContentType;
 
 @QuarkusTest
 class SimpleProjectHookTest {
+
+	private static final String REQUEST_BODY = """
+			{
+			"timestamp": 1727087443918,
+			"webhookEvent": "jira:issue_updated",
+			"issue_event_type_name": "issue_generic",
+			"user": {
+				"self": "https://hibernate.atlassian.net/rest/api/2/user?accountId=557058%3A18cf44bb-bc9b-4e8d-b1b7-882969ddc8e5",
+				"accountId": "557058:18cf44bb-bc9b-4e8d-b1b7-882969ddc8e5"
+			},
+			"issue": {
+				"id": "77256",
+				"self": "https://hibernate.atlassian.net/rest/api/2/77256",
+				"key": "JIRATEST1-14"
+			}
+			}
+			""";
+
 	@Test
 	void unknown() {
-		given().when()
-				.body("""
-						{
-						"timestamp": 1727087443918,
-						"webhookEvent": "jira:issue_updated",
-						"issue_event_type_name": "issue_generic",
-						"user": {
-							"self": "https://hibernate.atlassian.net/rest/api/2/user?accountId=557058%3A18cf44bb-bc9b-4e8d-b1b7-882969ddc8e5",
-							"accountId": "557058:18cf44bb-bc9b-4e8d-b1b7-882969ddc8e5"
-						},
-						"issue": {
-							"id": "77256",
-							"self": "https://hibernate.atlassian.net/rest/api/2/77256",
-							"key": "JIRATEST1-14"
-						}
-						}
-						""")
-				.contentType(ContentType.JSON).post("api/jira/webhooks/NOTAPROJECTKEY").then().statusCode(500)
-				.body(containsString(
+		given().when().body(REQUEST_BODY).contentType(ContentType.JSON).post("api/jira/webhooks/NOTAPROJECTKEY").then()
+				.statusCode(500).body(containsString(
 						"Unable to determine handler context for project NOTAPROJECTKEY. Was it not configured"));
 	}
 
 	@Test
 	void known() {
-		given().when()
-				.body("""
-						{
-						"timestamp": 1727087443918,
-						"webhookEvent": "jira:issue_updated",
-						"issue_event_type_name": "issue_generic",
-						"user": {
-							"self": "https://hibernate.atlassian.net/rest/api/2/user?accountId=557058%3A18cf44bb-bc9b-4e8d-b1b7-882969ddc8e5",
-							"accountId": "557058:18cf44bb-bc9b-4e8d-b1b7-882969ddc8e5"
-						},
-						"issue": {
-							"id": "1",
-							"self": "https://hibernate.atlassian.net/rest/api/2/1",
-							"key": "JIRATEST1-1"
-						}
-						}
-						""")
+		given().when().body(REQUEST_BODY)
+				.header("x-hub-signature", RequestSignatureFilter.sign("not-a-secret", REQUEST_BODY))
 				.contentType(ContentType.JSON).post("api/jira/webhooks/JIRATEST1").then().statusCode(200)
 				.body(is("ack"));
+	}
+
+	@Test
+	void knownNoHeader() {
+		given().when().body(REQUEST_BODY).contentType(ContentType.JSON).post("api/jira/webhooks/JIRATEST1").then()
+				.statusCode(401);
 	}
 
 }
