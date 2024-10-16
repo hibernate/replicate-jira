9b73294434e077136b170191bb7b485f55b7a1ff
marko-bekhta <marko.prykladna@gmail.com>
2024-10-16 19:16:56 +0200

    Start updating readme with some info and instructions

--- src/main/java/org/hibernate/infra/replicate/jira/ProcessingConfig.java
+++ src/main/java/org/hibernate/infra/replicate/jira/ProcessingConfig.java
@@ -5,9 +5,19 @@ import io.smallrye.config.WithDefault;
 
 @ConfigMapping(prefix = "processing.events")
 public interface ProcessingConfig {
+	/**
+	 * Define how many events can be acknowledged and put on the pending queue before
+	 * acknowledging an event results in blocking the response and waiting for the queue to free some space.
+	 */
 	@WithDefault("10000")
 	int queueSize();
 
+	/**
+	 * Define the number of threads to use when processing queued events.
+	 * <p>
+	 * Note, having a lot of processing threads might not bring much benefit as processing
+	 * may also be limited by {@link JiraConfig.EventProcessing}
+	 */
 	@WithDefault("2")
 	int threads();
 }
