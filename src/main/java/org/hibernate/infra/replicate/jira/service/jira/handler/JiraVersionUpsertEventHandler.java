1cb63ca3928683535cdba4cf2f0ae513eee8c052
marko-bekhta <marko.prykladna@gmail.com>
2024-11-19 11:32:48 +0100

    Use a cached map of fix versions when syncing the issues

--- src/main/java/org/hibernate/infra/replicate/jira/service/jira/handler/JiraVersionUpsertEventHandler.java
+++ src/main/java/org/hibernate/infra/replicate/jira/service/jira/handler/JiraVersionUpsertEventHandler.java
@@ -1,9 +1,7 @@
 package org.hibernate.infra.replicate.jira.service.jira.handler;
 
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
-import java.util.regex.Pattern;
 
 import org.hibernate.infra.replicate.jira.service.jira.HandlerProjectContext;
 import org.hibernate.infra.replicate.jira.service.jira.model.rest.JiraVersion;
@@ -20,46 +18,14 @@ public class JiraVersionUpsertEventHandler extends JiraEventHandler {
 		JiraVersion version = context.sourceJiraClient().version(objectId);
 		List<JiraVersion> downstreamVersions = context.destinationJiraClient().versions(context.project().projectKey());
 
-		JiraVersion send = new JiraVersion();
-		send.name = version.name;
-		send.description = prepareVersionDescription(version);
-		send.projectId = context.project().projectId();
+		JiraVersion send = version.copyForProject(context.project());
 
-		Optional<JiraVersion> found = findVersion(version.id, downstreamVersions);
+		Optional<JiraVersion> found = JiraVersion.findVersion(version.id, downstreamVersions);
 		if (found.isPresent()) {
 			context.destinationJiraClient().update(found.get().id, send);
 		} else {
 			context.destinationJiraClient().create(send);
 		}
-	}
-
-	private String prepareVersionDescription(JiraVersion version) {
-		URI verisonUri = createJiraVersionUri(version);
-		String content = """
-				{quote}This [version|%s] was created as a copy of %s{quote}
-
-
-				%s""".formatted(verisonUri, version.name, version.description);
-		return truncateContent(content);
-	}
-
-	protected Optional<JiraVersion> findVersion(String versionId, List<JiraVersion> versions) {
-		if (versions == null || versions.isEmpty()) {
-			return Optional.empty();
-		}
-		for (JiraVersion check : versions) {
-			if (hasRequiredVersionQuote(check.description, versionId)) {
-				return Optional.of(check);
-			}
-		}
-		return Optional.empty();
-	}
-
-	private boolean hasRequiredVersionQuote(String body, String versionId) {
-		// e.g. https://hibernate.atlassian.net/projects/HSEARCH/versions/32220/
-
-		return Pattern.compile("(?s)^\\{quote\\}This \\[version.+/versions/%s\\].*".formatted(versionId)).matcher(body)
-				.matches();
 	}
 
 	@Override
