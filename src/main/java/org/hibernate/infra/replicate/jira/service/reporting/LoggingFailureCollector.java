eadc7d10110b065908c39e1ab29be1f8d0013539
marko-bekhta <marko.prykladna@gmail.com>
2024-11-29 14:53:08 +0100

    "Fix" logger from failing to print a message

--- src/main/java/org/hibernate/infra/replicate/jira/service/reporting/LoggingFailureCollector.java
+++ src/main/java/org/hibernate/infra/replicate/jira/service/reporting/LoggingFailureCollector.java
@@ -11,26 +11,30 @@ class LoggingFailureCollector implements FailureCollector {
 
 	@Override
 	public void warning(String details) {
-		Log.warn(details);
+		Log.warn(escape(details));
 	}
 
 	@Override
 	public void warning(String details, Exception exception) {
-		Log.warnf(exception, details);
+		Log.warnf(exception, escape(details));
 	}
 
 	@Override
 	public void critical(String details) {
-		Log.error(details);
+		Log.error(escape(details));
 	}
 
 	@Override
 	public void critical(String details, Exception exception) {
-		Log.errorf(exception, details);
+		Log.errorf(exception, escape(details));
 	}
 
 	@Override
 	public void close() {
 		// do nothing, we've already logged all
+	}
+
+	private String escape(String s) {
+		return s.replace('{', '[').replace('}', ']');
 	}
 }
